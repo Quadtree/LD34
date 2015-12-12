cdaf959d642adfe6f0c0b9a77eac3e5fe969ce61
Eric Brown <quadtree@gmail.com>
2015-12-12 13:46:58 -0800

    Over wiggly mouse control

--- Source/LD34/Grid.cpp
+++ Source/LD34/Grid.cpp
@@ -26,7 +26,46 @@ void AGrid::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
 
-	//UE_LOG(LogTemp, Display, TEXT("Pos %s"), *GetActorLocation().ToString());
+	//UE_LOG(LogTemp, Display, TEXT("Pos %s"), *Destination.ToString());
+
+	//FVector2D delta = (Destination - FVector2D(GetActorLocation().X, GetActorLocation().Y));
+
+	//float angleToTarget = FMath::RadiansToDegrees(FMath::Atan2(delta.Y, delta.X));
+
+	//float curAngle = GetActorRotation().Yaw;
+
+	//float newAngle = FMath::FixedTurn(curAngle, angleToTarget, 1);
+
+	if (DestinationMode)
+	{
+		float curAngle = FMath::DegreesToRadians(GetActorRotation().Yaw);
+
+		FVector2D pointCenter(GetActorLocation().X, GetActorLocation().Y);
+
+		FVector2D pointLeft = pointCenter + FVector2D(FMath::Cos(curAngle - 0.22f), FMath::Sin(curAngle - 0.22f))*200;
+		FVector2D pointMiddle = pointCenter + FVector2D(FMath::Cos(curAngle), FMath::Sin(curAngle)) * 200;
+		FVector2D pointRight = pointCenter + FVector2D(FMath::Cos(curAngle + 0.22f), FMath::Sin(curAngle + 0.22f)) * 200;
+
+		UE_LOG(LogTemp, Display, TEXT("Pos %s"), *pointMiddle.ToString());
+
+		DrawDebugSphere(GetWorld(), FVector(pointLeft.X, pointLeft.Y, 200), 50, 4, FColor::Red, false, 1);
+		DrawDebugSphere(GetWorld(), FVector(pointMiddle.X, pointMiddle.Y, 200), 50, 4, FColor::Green, false, 1);
+		DrawDebugSphere(GetWorld(), FVector(pointRight.X, pointRight.Y, 200), 50, 4, FColor::Blue, false, 1);
+
+		float leftDist = FVector2D::DistSquared(pointLeft, Destination);
+		float middleDist = FVector2D::DistSquared(pointMiddle, Destination);
+		float rightDist = FVector2D::DistSquared(pointRight, Destination);
+
+		if (leftDist < middleDist && leftDist < rightDist)
+		{
+			LeftRightTurn = -1;
+		}
+
+		if (rightDist < middleDist && rightDist < leftDist)
+		{
+			LeftRightTurn = 1;
+		}
+	}
 }
 
 // Called to bind functionality to input
